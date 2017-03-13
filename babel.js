72c09c71db6389bb47ef52cbe4894b065c26bcf2
Filip Malinowski <Filip.Malinowski@powel.com>
2017-03-13 09:49:00 +0100

    fix deprecated babel rules

--- babel.js
+++ babel.js
@@ -10,16 +10,12 @@ module.exports = {
     'strict': [2, 'never'],
     'no-var': 2,
     'prefer-const': 2,
-    'generator-star-spacing': 0,
-    'array-bracket-spacing': 0,
-    'object-shorthand': 0,
-    'arrow-parens': 0,
+    'generator-star-spacing': [2, 'after'],
+    'array-bracket-spacing': [1, 'never'],
+    'object-shorthand': [1, 'always'],
+    'arrow-parens': [1, 'as-needed'],
 
     // https://github.com/babel/eslint-plugin-babel
-    'babel/generator-star-spacing': [2, 'after'],
-    'babel/object-curly-spacing': [1, 'always'],
-    'babel/array-bracket-spacing': [1, 'never'],
-    'babel/object-shorthand': [1, 'always'],
-    'babel/arrow-parens': [1, 'as-needed']
+    'babel/object-curly-spacing': [1, 'always']
   }
 };
